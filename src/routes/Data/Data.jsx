9ff138afc521b42dba4d9e9db4baad9a38cfdcb0
Adrian Samtani <busanta29@gmail.com>
2023-01-09 01:24:12 +0800

    missing changes completed

--- src/routes/Data/Data.jsx
+++ src/routes/Data/Data.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import '../Data/Data.css';
 import Navbar from '../../components/navbar/Navbar';
 import Datahub from '../../components/datahub/Datahub';
-import Datapdf from '../../components/datapdf/Datapdf';
 import Footer from '../../components/footer/Footer';
 
 const Data = () => {
@@ -11,7 +10,6 @@ const Data = () => {
         <Navbar/>
         <h1>DATA ANALYSIS</h1>
         <Datahub/>
-        <Datapdf/>
         <Footer/>
     </div>
   )
