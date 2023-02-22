b5fdcec56e65dd595118206634eb9036b8b276ed
Adrian Samtani <busanta29@gmail.com>
2023-02-22 15:31:06 +0800

    installed cloud functions

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require("firebase-functions");
+
+exports.mailTrigger = functions.firestore.document('/bookings/{id}') 
+    .onCreate((snap, context) => {
+        console.log(snap.data());
+
+        const bookings = context.params.bookings;
+        const id = context.params.id;
+
+        const mail = admin.firestore().collection('mail');
+
+        return mail.add({id})
+    })
