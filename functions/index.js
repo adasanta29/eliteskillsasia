8ab79c554ff7053be7c5ecb3528a38511c7039ed
Adrian Samtani <busanta29@gmail.com>
2023-02-22 17:34:50 +0800

    mnm functions

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,3 @@
-const functions = require("firebase-functions");
-const admin = require ("firebase-admin");
-
-exports.mailTrigger = functions.firestore.document("/{collection}/{id}")
-    .onCreate((snap, context) => {
-        console.log(snap.data());
-
-        const collection = context.params.collection;
-        const id = context.params.id;
-
-        const mail = admin.firestore().collection("mail");
-
-        if (collection === "bookings") {
-            return mail.add({ text: "A new booking was made"});
-        }
-
-        return null;
-    })
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -40,3 +21,22 @@ export const analytics = getAnalytics(app);
 
 // "site": "eliteskillsasia",
 // "build": "react-scripts build",
+
+const functions = require("firebase-functions");
+const admin = require ("firebase-admin");
+
+exports.mailTrigger = functions.firestore.document("/{collection}/{id}")
+    .onCreate((snap, context) => {
+        console.log(snap.data());
+
+        const collection = context.params.collection;
+        const id = context.params.id;
+
+        const mail = admin.firestore().collection("mail");
+
+        if (collection === "bookings") {
+            return mail.add({ text: "A new booking was made"});
+        }
+
+        return null;
+    })
